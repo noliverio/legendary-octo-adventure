1f31914222c7c102587b665a51a33018563b0b14
Nick Oliverio <nick@nicholasoliverio.com>
2017-08-14 02:01:17 -0400

    Initial commit

---
+++ var_example.java
@@ -0,0 +1,23 @@
+/*
+This demonstrates a variable
+
+call the class example2
+*/
+
+class example{
+    public static void main(String args[]){
+        //declare variable
+        int var1;
+        int var2;
+
+        //assign a value to the variable
+        var1 = 1024;
+
+        System.out.println("Var1 contains: "+var1);
+
+        var2 = var1/2;
+
+        System.out.print("var2 contains var1 / 2 :");
+        System.out.println(var2);
+    }
+}
