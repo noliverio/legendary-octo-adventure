5bd3a1044bcd8a55a4bc17e6961ef99ff879e6a1
noliverio <nick@nicholasoliverio.com>
2017-08-26 01:24:03 -0400

    continues working through chapter two of java beginner's guide.

--- beginners_guide/ScopeDemo.java
+++ beginners_guide/ScopeDemo.java
@@ -2,16 +2,19 @@
 
 class ScopeDemo{
     public static void main(String args[]){
-        int x;
+        int x; // visible to all code within main
 
         x = 10;
-        if (x == 10){
-            int y = 20;
+        if (x == 10){ //start a new scope
+            int y = 20; //only visible within this code block
 
+            // x and y are both visible here
             System.out.println("x and y: " + x + " " + y);
             x = y * 2;
         }
+        // y = 100; //"Error can not find sysmbol" y not known here
 
+        //x is still visible
         System.out.println("x is: " + x);
     }
 }
