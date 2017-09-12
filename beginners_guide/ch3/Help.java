f108d51fbaede925000391ce9f2af424bd94ced5
noliverio <nick@nicholasoliverio.com>
2017-09-11 22:07:09 -0400

    Continued working through ch3

---
+++ beginners_guide/ch3/Help.java
@@ -0,0 +1,36 @@
+// Java Help System
+
+class Help{
+    public static void main(String args[])
+        throws java.io.IOException {
+        
+        char choice;
+
+        System.out.println("Help on:");
+        System.out.println("  1. if");
+        System.out.println("  2. switch");
+        System.out.print("Chose one: ");
+
+        choice = (char) System.in.read();
+
+        switch(choice){
+            case '1':
+                System.out.println("The if:\n");
+                System.out.println("if(condition) statement;");
+                System.out.println("else statement;");
+                break;
+            case '2':
+                System.out.println("The switch:\n");
+                System.out.println("switch(expression) {");
+                System.out.println("    case constant:");
+                System.out.println("        statment sequence");
+                System.out.println("        break");
+                System.out.println("    // ...");
+                System.out.println("}");
+                break;
+            default:
+                System.out.println("Selection not found.");
+
+        }
+    }
+}
