9de68b6743946388cc904868ef2be097f0874796
noliverio <nick@nicholasoliverio.com>
2017-09-18 23:49:42 -0400

    Began reading and working through ch 5

---
+++ beginners_guide/ch5/ArrayDemo.java
@@ -0,0 +1,15 @@
+// Demo one-d array in java
+
+class ArrayDemo{
+    public static void main(String args[]){
+        int sample[] = new int[10];
+        int i;
+
+        for (i=0; i<10; i++)
+            sample[i] = i;
+
+        for(i=0; i<10; i++)
+            System.out.println("This is sample[" + i + "]: "
+                              + sample[i]);
+    }
+}
